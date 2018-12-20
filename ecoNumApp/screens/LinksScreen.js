8ed55ad54c55c73919111ab9b01a6966e9225e8d
Camille <camille.coutens@gmail.com>
2018-12-20 20:37:55 +0100

    premier élements de l'interface je compare

--- ecoNumApp/screens/LinksScreen.js
+++ ecoNumApp/screens/LinksScreen.js
@@ -1,18 +1,34 @@
-import React from 'react';
-import { ScrollView, StyleSheet } from 'react-native';
-import { ExpoLinksView } from '@expo/samples';
+import React from "react";
+import { ScrollView, StyleSheet, Text, Image } from "react-native";
 
 export default class LinksScreen extends React.Component {
+  // static navigationOptions = {
+  //   headerTitle: (
+  //     <Image
+  //       source={require("../img/LogoEcoNum.png")}
+  //       style={{ width: 60, height: 60, marginBottom: 15 }}
+  //     />
+  //   ),
+  // };
   static navigationOptions = {
-    title: 'Links',
+    headerTitle: (
+      <Image
+        source={require("../img/LogoEcoNum.png")}
+        style={{ width: 156, height: 51, marginBottom: 15 }}
+      />
+    )
   };
-
   render() {
     return (
       <ScrollView style={styles.container}>
-        {/* Go ahead and delete ExpoLinksView and replace it with your
-           * content, we just wanted to provide you with some helpful links */}
-        <ExpoLinksView />
+        <Image
+          source={require("../img/phoneImage.jpg")}
+          style={{
+            width: 418,
+            height: 288
+          }}
+        />
+        <Text style={styles.titleCompare}> Je compare mon téléphone </Text>
       </ScrollView>
     );
   }
@@ -22,6 +38,12 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     paddingTop: 15,
-    backgroundColor: '#fff',
+    backgroundColor: "#fff"
   },
+  titleCompare: {
+    textAlign: "center",
+    marginTop: 10,
+    fontSize: 18,
+    fontWeight: "bold"
+  }
 });
