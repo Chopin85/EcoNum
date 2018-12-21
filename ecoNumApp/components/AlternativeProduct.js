edfc431c182a70ea14d084594b0bab8b4c06db01
Prudence Gaboriau <prudence.gaboriau@gmail.com>
2018-12-21 12:26:59 +0100

    page alternatives css

--- ecoNumApp/components/AlternativeProduct.js
+++ ecoNumApp/components/AlternativeProduct.js
@@ -1,11 +1,39 @@
 import React, { Component } from "react";
 import { View, Text, Image, StyleSheet } from "react-native";
 import Pie from "react-native-pie";
+import AwesomeButton from "react-native-really-awesome-button";
 
 class AlternativeProduct extends Component {
-  state = {};
+  state = {
+    pieColor: "green"
+  };
+
+  getColorPie = () => {
+    if (this.props.product.note > 35) {
+      this.setState({
+        pieColor: "red"
+      });
+      if (this.props.product.note >= 35 || this.props.product.note < 70) {
+        this.setState({
+          pieColor: "orange"
+        });
+      }
+      if (this.props.product.note >= 70) {
+        this.setState({
+          pieColor: "green"
+        });
+      }
+    }
+  };
+  componentDidMount() {
+    this.getColorPie();
+  }
+  handlePress = () => {
+    console.log("ok");
+  };
 
   render() {
+    console.log(this.props.product.note);
     const { product } = this.props;
     console.log("product image", product.image);
     const {
@@ -22,37 +50,80 @@ class AlternativeProduct extends Component {
       image
     } = product;
     return (
-      <View style={{ flex: 1, flexDirection: "row" }}>
-        <Image
-          source={require("../img/iphonex.png")}
-          style={{ width: 100, height: 150, marginLeft: 45 }}
-        />
-        <Pie
-          radius={50}
-          innerRadius={45}
-          series={[note]}
-          colors={["#f00"]}
-          backgroundColor="#ddd"
-        />
-        <View style={styles.gauge}>
-          <Text style={styles.gaugeText}>{note}%</Text>
+      <View style={styles.container}>
+        <View style={styles.imageContainer}>
+          <Image
+            source={require("../img/iphonex.png")}
+            style={{ width: 100, height: 150 }}
+          />
+        </View>
+        <View style={styles.child}>
+          <Text style={styles.titleText}>{designation}</Text>
+          <View style={styles.pie}>
+            <Pie
+              radius={30}
+              innerRadius={25}
+              series={[note]}
+              colors={[this.state.pieColor]}
+              backgroundColor="#ddd"
+            />
+            <View style={styles.gauge}>
+              <Text style={styles.gaugeText}>{note}%</Text>
+            </View>
+          </View>
+          <View style={{ marginTop: 5 }}>
+            <AwesomeButton
+              onPress={this.handlePress}
+              backgroundColor="#75a579"
+              width={120}
+              height={25}
+            >
+              en savoir plus
+            </AwesomeButton>
+          </View>
         </View>
       </View>
     );
   }
 }
 const styles = StyleSheet.create({
+  container: {
+    display: "flex",
+    flexDirection: "row",
+    justifyContent: "center",
+    alignItems: "center"
+  },
+  child: {
+    marginTop: 10,
+    marginBottom: 10,
+    marginRight: 10,
+    marginLeft: 10,
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center"
+  },
+  pie: {
+    position: "relative",
+    marginTop: 5,
+  },
+  imageContainer: {
+    marginTop: 10,
+    marginBottom: 10,
+    marginRight: 10,
+    marginLeft: 10
+  },
   gauge: {
     position: "absolute",
-    width: 100,
-    height: 100,
-    alignItems: "center",
-    justifyContent: "center"
+    right: 5,
+    bottom: 15
+  },
+  titleText: {
+    fontSize: 18
   },
   gaugeText: {
     backgroundColor: "transparent",
     color: "#000",
-    fontSize: 24
+    fontSize: 22
   }
 });
 
