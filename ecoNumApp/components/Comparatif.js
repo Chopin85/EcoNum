576b6b4929eacabaa95e6d29d3c82d5479d1eeea
Camille <camille.coutens@gmail.com>
2018-12-21 03:20:26 +0100

    petit tableau avec les prix

--- ecoNumApp/components/Comparatif.js
+++ ecoNumApp/components/Comparatif.js
@@ -1,5 +1,5 @@
 import React, { Component, Fragment } from "react";
-import { View, Text } from "react-native";
+import { View, Text, StyleSheet } from "react-native";
 import Product from "./Product";
 import { data } from "../data";
 
@@ -51,8 +51,36 @@ export default class Comparatif extends Component {
             <Text style={{ fontSize: 10 }}>{product2.fabricant}</Text>
           </View>
         </View>
-        <Text style={{ textAlign: "center" }}>Prix de vente Ttc</Text>
+        <Text style={{ textAlign: "center", marginTop: 10 }}>
+          Prix de vente TTC
+        </Text>
+        <View style={styles.lineStyle} />
+
+        <View style={{ flex: 1, flexDirection: "row", marginTop: 20 }}>
+          <View style={{ flex: 1 }}>
+            <Text style={{ textAlign: "center" }}>{product1.prixTTC}</Text>
+          </View>
+          <View style={styles.vertical} />
+          <View style={{ flex: 1 }}>
+            <Text style={{ textAlign: "center" }}>{product2.prixTTC}</Text>
+          </View>
+        </View>
       </Fragment>
     );
   }
 }
+
+const styles = StyleSheet.create({
+  lineStyle: {
+    borderWidth: 0.5,
+    borderColor: "black",
+    marginTop: 10,
+    marginBottom: -15
+  },
+  vertical: {
+    borderWidth: 0.5,
+    borderColor: "black",
+    width: 1,
+    height: 30
+  }
+});
