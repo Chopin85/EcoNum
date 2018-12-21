777653413c6b08bd73fd85172b25508be58f66fe
Camille <camille.coutens@gmail.com>
2018-12-21 02:12:17 +0100

    complément comparaison

--- ecoNumApp/components/Product.js
+++ ecoNumApp/components/Product.js
@@ -1,74 +1,47 @@
-import React, {
-  Component,
-} from 'react';
-import {
-  View, Text
-} from 'react-native';
-import {
-  data
-} from '../data'
-
-const category = "smartphone"
-const mark = "Apple"
-const model = "Iphone X"
+import React, { Component } from "react";
+import { View, Text } from "react-native";
 
 class Product extends Component {
   state = {
-    product: {
-      designation: "",
-      fabricant: "",
-      categorie: ""
-    },
+    product: {},
     loaded: false
-  }
-  getProduct (category, mark, model) {
-    console.log(data[0].categorie);
-    console.log(data[0].fabricant);
-    console.log(data[0].designation);
-    const product = data.map(element => {
-      if (element.categorie === category && element.fabricant === mark && element.designation === model) {
-        console.log("mon element", element);
+  };
 
-        return element;
-      }
-    })
-    return product[0]
+  componentDidMount() {
+    this.setState({
+      product: this.props.product,
+      loaded: true
+    });
   }
 
-  componentDidMount () {
-    this.setState({ product: this.getProduct(this.props.category, this.props.mark, this.props.model), loaded: true })
-  }
-
-  render () {
+  render() {
     const { loaded, product } = this.state;
-    const { categorie, designation, fabricant,prixTTC,batterie,resolution,deee,classeEnergie,recyclable,note } = product;
-    return loaded ? (<View>
-      <Text>
-        {categorie}
-      </Text>
-      <Text>
-        {fabricant}
-      </Text>
-      <Text>
-        {designation}
-      </Text>
-      <Text>
-        prix :{prixTTC} euros
-      </Text>
-      <Text>
-        Resulution :{resolution}
-      </Text>
-      <Text>
-        Classe enrgie :{classeEnergie}
-      </Text>
-      <Text>
-        Classe enrgie :{classeEnergie}
-      </Text>
-    </View>) : <Text>
-        loading ..
-      </Text>
+    const {
+      categorie,
+      designation,
+      fabricant,
+      prixTTC,
+      batterie,
+      resolution,
+      deee,
+      classeEnergie,
+      recyclable,
+      note
+    } = product;
+    return loaded ? (
+      <View>
+        <Text>{categorie}</Text>
+        <Text>{fabricant}</Text>
+        <Text>{designation}</Text>
+        <Text>prix :{prixTTC} euros</Text>
+        <Text>Resulution :{resolution}</Text>
+        <Text>Classe enrgie :{classeEnergie}</Text>
+        <Text>Classe enrgie :{classeEnergie}</Text>
+      </View>
+    ) : (
+      <Text>loading ..</Text>
+    );
   }
-
 }
 
 export default Product;
